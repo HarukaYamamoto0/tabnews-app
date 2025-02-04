97b6e6b3e3bd1eea495e080b64396fa00e4fc342
HarukaYamamoto0 <harukayamamotodev@gmail.com>
2025-02-04 11:35:22 -0300

    feat: temporary bottom bar created

--- app/src/main/java/com/harukadev/tabnews/core/navigation/CustomBottomAppBar.kt
+++ app/src/main/java/com/harukadev/tabnews/core/navigation/CustomBottomAppBar.kt
@@ -1,10 +1,15 @@
 package com.harukadev.tabnews.core.navigation
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Badge
 import androidx.compose.material3.BadgedBox
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemColors
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -12,10 +17,13 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.harukadev.tabnews.R
@@ -34,51 +42,67 @@ data class BottomNavigationItem(
 fun CustomBottomAppBar(navController: NavHostController, modifier: Modifier = Modifier) {
     val items = listOf(
         BottomNavigationItem(
+            title = stringResource(R.string.recents),
+            selectedIcon = ImageVector.vectorResource(R.drawable.house_fill),
+            unselectedIcon = ImageVector.vectorResource(R.drawable.house),
+            hasNews = false,
+            badgeCount = null,
+            RecentPostsRouter
+        ),
+        BottomNavigationItem(
             title = stringResource(R.string.relevant),
             selectedIcon = ImageVector.vectorResource(R.drawable.ranking_fill),
             unselectedIcon = ImageVector.vectorResource(R.drawable.ranking),
             hasNews = false,
             badgeCount = null,
             router = RelevantPostsRouter
-        ),
-        BottomNavigationItem(
-            title = stringResource(R.string.recents),
-            selectedIcon = ImageVector.vectorResource(R.drawable.clockwise_fill),
-            unselectedIcon = ImageVector.vectorResource(R.drawable.clockwise),
-            hasNews = false,
-            badgeCount = null,
-            RecentPostsRouter
-        ),
+        )
     )
 
     var selectedItemIndex by rememberSaveable {
         mutableIntStateOf(0)
     }
 
-    NavigationBar {
+    NavigationBar(
+        containerColor = MaterialTheme.colorScheme.tertiary,
+        tonalElevation = 5.dp,
+        modifier = modifier
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(7.dp))
+    ) {
         items.forEachIndexed { index, item ->
-            NavigationBarItem(selected = selectedItemIndex == index, onClick = {
-                selectedItemIndex = index
-                navController.navigate(item.router)
-            }, label = {
-                Text(item.title)
-            }, icon = {
-                BadgedBox(badge = {
-                    if (item.badgeCount != null) {
-                        Badge {
-                            Text(item.badgeCount.toString())
+            NavigationBarItem(
+                selected = selectedItemIndex == index,
+                onClick = {
+                    selectedItemIndex = index
+                    navController.navigate(item.router)
+                },
+//                label = {
+//                    Text(item.title)
+//                },
+                icon = {
+                    BadgedBox(badge = {
+                        if (item.badgeCount != null) {
+                            Badge {
+                                Text(item.badgeCount.toString())
+                            }
+                        } else if (item.hasNews) {
+                            Badge()
                         }
-                    } else if (item.hasNews) {
-                        Badge()
+                    }) {
+                        Icon(
+                            imageVector = if (selectedItemIndex == index) item.selectedIcon
+                            else item.unselectedIcon,
+                            contentDescription = item.title
+                        )
                     }
-                }) {
-                    Icon(
-                        imageVector = if (selectedItemIndex == index) item.selectedIcon
-                        else item.unselectedIcon,
-                        contentDescription = item.title
-                    )
-                }
-            })
+                },
+                colors = NavigationBarItemDefaults.colors(
+                    indicatorColor = Color.Transparent,
+                    selectedIconColor = Color.White,
+                    unselectedIconColor = Color.White,
+                )
+            )
         }
     }
 }
